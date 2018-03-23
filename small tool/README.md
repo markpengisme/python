a94fdeff5cba2dea1247b262afeacccde1617550
Mark Peng <markpengisme@gmail.com>
2018-03-24 02:07:21 +0800

    add case conversion tool

---
+++ small tool/README.md
@@ -0,0 +1,3 @@
+# Small Tool
+### case_conversion.py
+- Simply enter your text and print upper, lower, capitalize  and title case .
---
+++ small tool/case_conversion.py
@@ -0,0 +1,9 @@
+def main():
+	str=input("Please input sentences:\n")
+	print('1.UPPER CASE:\t'+str.upper())
+	print('2.lower case:\t'+str.lower())
+	print('3.Capitalize Case:\t'+str.capitalize())
+	print('4.Title case:\t'+str.title())
+
+if __name__ == '__main__':
+	main()
